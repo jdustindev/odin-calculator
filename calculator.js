0a8a3db1729d8c82681c747865e63d71235a756e
jdustindev <jdustindev@gmail.com>
2022-12-07 12:54:29 -0500

    Add operate() function
    operate() takes an operator and 2 numbers, and returns the result of
    performing the appropriate operation.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return Number(a) * Number(b);
 }
+
+function operate(operator, a, b) {
+    let result;
+    switch(operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
+
+console.log(operate('+', "10", "5"));
+console.log(operate('-', 10, 5));
+console.log(operate('*', 10, 5));
+console.log(operate('/', 10, 5));
